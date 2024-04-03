9c45c7f21a09d42ddee62699e8299bdb819f3e8b
Andreas Zweili <andreas@zweili.ch>
2024-04-04 00:22:21 +0200

    Rewrite to flake-utils

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -13,65 +14,60 @@
       self,
       nixpkgs,
       devenv,
+      flake-utils,
       poetry2nix,
     }@inputs:
-    let
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      forEachSystem = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
-      devShells = forEachSystem (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          config = self.devShells.${system}.default.config;
-          env = mkPoetryEnv {
-            projectDir = ./.;
-            python = pkgs.python312;
-          };
-          inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
-          tests = pkgs.writeShellScriptBin "python-test" ''
-            trap "process-compose down &> /dev/null" EXIT
-            process-compose up --tui=false &
-            pytest --cov=src tests.py
-          '';
-        in
-        {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              {
-                env = {
-                  NO_SSL = "True";
-                  PC_PORT_NUM = "9999";
-                };
-                enterShell = ''
-                  ln -sf ${config.process-managers.process-compose.configFile} ${config.env.DEVENV_ROOT}/process-compose.yml
-                '';
-                packages = [
-                  env
-                  tests
-                ];
-                process-managers.process-compose.enable = true;
-                processes = {
-                  webserver = {
-                    process-compose.depends_on.redis.condition = "process_started";
-                    exec = "gunicorn src.main:app";
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+      in
+      {
+        packages = {
+          devenv-up = self.devShells.${system}.default.config.procfileScript;
+        };
+        devShells =
+          let
+            config = self.devShells.${system}.default.config;
+            env = poetry2nix.mkPoetryEnv {
+              projectDir = ./.;
+              python = pkgs.python312;
+            };
+            tests = pkgs.writeShellScriptBin "python-test" ''
+              trap "process-compose down &> /dev/null" EXIT
+              process-compose up --tui=false &
+              pytest --cov=src tests.py
+            '';
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  env = {
+                    NO_SSL = "True";
+                    PC_PORT_NUM = "9999";
                   };
-                };
-                services.redis.enable = true;
-              }
-            ];
+                  enterShell = ''
+                    ln -sf ${config.process-managers.process-compose.configFile} ${config.env.DEVENV_ROOT}/process-compose.yml
+                  '';
+                  packages = [
+                    env
+                    tests
+                  ];
+                  process-managers.process-compose.enable = true;
+                  processes = {
+                    webserver = {
+                      process-compose.depends_on.redis.condition = "process_started";
+                      exec = "gunicorn src.main:app";
+                    };
+                  };
+                  services.redis.enable = true;
+                }
+              ];
+            };
           };
-        }
-      );
-    };
+      }
+    );
 }
