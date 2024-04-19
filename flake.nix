369ec3508319234db52239fe553abcb7a3301703
Andreas Zweili <andreas@zweili.ch>
2024-04-19 19:42:41 +0200

    Add matching redis image

--- flake.nix
+++ flake.nix
@@ -49,6 +49,17 @@
               ];
             };
           };
+          redis-image = pkgs.dockerTools.buildImage {
+            name = "redis";
+            tag = "latest";
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              paths = [ pkgs.redis ];
+            };
+            config = {
+              Cmd = [ "${pkgs.redis}/bin/redis-server" ];
+            };
+          };
         };
         devShells =
           let
