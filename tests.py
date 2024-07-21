f11669ac52e3cccd2588058d65142ceb7a169607
Andreas Zweili <andreas@zweili.ch>
2024-07-21 18:42:11 +0200

    Update tests

--- tests.py
+++ tests.py
@@ -7,33 +7,32 @@ from cryptography.fernet import Fernet
 from freezegun import freeze_time
 from werkzeug.exceptions import BadRequest
 
-# noinspection PyPep8Naming
-import snapbin.main as snapbin
+import snapbin.main as snap
+from snapbin.models import Secret
 
 
 def test_get_password(memory_db):
     password = "melatonin overdose 1337!$"
-    key = snapbin.set_password(password, 30)
-    assert password == snapbin.get_password(key)
-    # Assert that we can't look this up a second time.
-    assert snapbin.get_password(key) == None
+    key = snap.set_password(password, 30)
+    assert password == snap.get_password(key), "passwords do not match"
+    assert snap.get_password(key) is None, "password should be expired"
 
 
 def test_password_is_not_stored_in_plaintext():
     password = "trustno1"
-    token = snapbin.set_password(password, 30)
-    redis_key = token.split(snapbin.TOKEN_SEPARATOR)[0]
-    stored_password_text = snapbin.redis_client.get(redis_key).decode("utf-8")
+    token = snap.set_password(password, 30)
+    redis_key = token.split(snap.TOKEN_SEPARATOR, maxsplit=1)[0]
+    stored_password_text = Secret.get(Secret.storage_key == redis_key).value
     assert stored_password_text not in password
 
 
 def test_returned_token_format():
     password = "trustsome1"
-    token = snapbin.set_password(password, 30)
-    token_fragments = token.split(snapbin.TOKEN_SEPARATOR)
+    token = snap.set_password(password, 30)
+    token_fragments = token.split(snap.TOKEN_SEPARATOR)
     assert 2 == len(token_fragments)
     redis_key, encryption_key = token_fragments
-    assert (32 + len(snapbin.REDIS_PREFIX)) == len(redis_key)
+    assert (32 + len(snap.REDIS_PREFIX)) == len(redis_key)
     try:
         Fernet(encryption_key.encode("utf-8"))
     except ValueError:
@@ -42,10 +41,10 @@ def test_returned_token_format():
 
 def test_encryption_key_is_returned():
     password = "trustany1"
-    token = snapbin.set_password(password, 30)
-    token_fragments = token.split(snapbin.TOKEN_SEPARATOR)
+    token = snap.set_password(password, 30)
+    token_fragments = token.split(snap.TOKEN_SEPARATOR)
     redis_key, encryption_key = token_fragments
-    stored_password = snapbin.redis_client.get(redis_key)
+    stored_password = snap.redis_client.get(redis_key)
     fernet = Fernet(encryption_key.encode("utf-8"))
     decrypted_password = fernet.decrypt(stored_password).decode("utf-8")
     assert password == decrypted_password
@@ -54,65 +53,65 @@ def test_encryption_key_is_returned():
 def test_unencrypted_passwords_still_work():
     unencrypted_password = "trustevery1"
     storage_key = uuid.uuid4().hex
-    snapbin.redis_client.setex(storage_key, 30, unencrypted_password)
-    retrieved_password = snapbin.get_password(storage_key)
+    snap.redis_client.setex(storage_key, 30, unencrypted_password)
+    retrieved_password = snap.get_password(storage_key)
     assert unencrypted_password == retrieved_password
 
 
 def test_password_is_decoded():
     password = "correct horse battery staple"
-    key = snapbin.set_password(password, 30)
-    assert not isinstance(snapbin.get_password(key), bytes)
+    key = snap.set_password(password, 30)
+    assert not isinstance(snap.get_password(key), bytes)
 
 
 def test_clean_input():
     # Test Bad Data
-    with snapbin.app.test_request_context(
+    with snap.app.test_request_context(
         "/", data={"password": "foo", "ttl": "bar"}, method="POST"
     ):
         with pytest.raises(BadRequest):
-            snapbin.clean_input()
+            snap.clean_input()
 
     # No Password
-    with snapbin.app.test_request_context("/", method="POST"):
+    with snap.app.test_request_context("/", method="POST"):
         with pytest.raises(BadRequest):
-            snapbin.clean_input()
+            snap.clean_input()
 
     # No TTL
-    with snapbin.app.test_request_context(
+    with snap.app.test_request_context(
         "/", data={"password": "foo", "ttl": ""}, method="POST"
     ):
-        assert (604800, "foo") == snapbin.clean_input()
+        assert (604800, "foo") == snap.clean_input()
 
-    with snapbin.app.test_request_context(
+    with snap.app.test_request_context(
         "/", data={"password": "foo", "ttl": 3600}, method="POST"
     ):
-        assert (3600, "foo") == snapbin.clean_input()
+        assert (3600, "foo") == snap.clean_input()
 
 
 def test_password_before_expiration():
     password = "fidelio"
-    key = snapbin.set_password(password, 1)
-    assert password == snapbin.get_password(key)
+    key = snap.set_password(password, 1)
+    assert password == snap.get_password(key)
 
 
 def test_password_after_expiration():
     password = "open sesame"
-    key = snapbin.set_password(password, 1)
+    key = snap.set_password(password, 1)
     time.sleep(1.5)
-    assert snapbin.get_password(key) == None
+    assert snap.get_password(key) is None
 
 
 def test_preview_password(app):
     password = "I like novelty kitten statues!"
-    key = snapbin.set_password(password, 30)
+    key = snap.set_password(password, 30)
     rv = app.get("/{0}".format(key))
     assert password not in rv.get_data(as_text=True)
 
 
 def test_show_password(app):
     password = "I like novelty kitten statues!"
-    key = snapbin.set_password(password, 30)
+    key = snap.set_password(password, 30)
     rv = app.post("/get-secret", data={"key": key})
     assert password in rv.get_data(as_text=True)
 
@@ -130,7 +129,7 @@ def test_set_password_json(app):
         key = json_content["key"]
 
         frozen_time.move_to("2020-05-22 11:59:59")
-        assert snapbin.get_password(key) == password
+        assert snap.get_password(key) == password
 
         frozen_time.move_to("2020-05-22 12:00:00")
-        assert snapbin.get_password(key) == None
+        assert snap.get_password(key) is None
